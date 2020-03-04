525dadb270826e344e2f008454050fa1495e813c
Oscar Gamonal <ogamonal@yahoo.com>
2020-03-03 19:11:01 -0500

    Last version.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 
 ## Write a short comment describing this function
 ## The makeCacherMatrix assign and returns the  values of a matrix 'x' and
-## its matrix inverse 
+## its matrix inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -18,6 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
+    
 }
 
 
